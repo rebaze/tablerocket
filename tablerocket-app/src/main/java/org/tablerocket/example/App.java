45b93ebfe774ce5af2b03ecacdf06b8895ab0fcb
Toni <toni@tablerocket.org>
2018-02-27 13:27:14 +0100

    initial version bootstrap using initial febo

---
+++ tablerocket-app/src/main/java/org/tablerocket/example/App.java
@@ -0,0 +1,20 @@
+package org.tablerocket.example;
+
+import org.tablerocket.core.FeboRepository;
+
+import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
+import static org.tablerocket.febo.core.Febo.febo;
+
+public class App
+{
+    public static void main( String[] args ) throws Exception
+    {
+        FeboRepository repo = new FeboRepository();
+
+        febo()
+            .require( repo.org_apache_felix_configadmin() )
+            .require( repo.org_apache_felix_scr() )
+            .with( "booking", bundle().add( BookingService.class ) )
+            .run( args );
+    }
+}
